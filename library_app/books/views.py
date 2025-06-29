ccf7e16b64fa36ae9929e04db1857feefeadc538
moro23 <billhab66@gmail.com>
2025-06-29 09:04:57 +0000

    add BookListView to display list of books

--- library_app/books/views.py
+++ library_app/books/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book
 
 # Create your views here.
+class BookListView(ListView):
+    model = Book 
+    template_name = 'books/book_list.html'
+
+
+    
