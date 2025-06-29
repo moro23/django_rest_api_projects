55cd27736a36f519a5d74e6b7017f751b01df7cc
moro23 <billhab66@gmail.com>
2025-06-29 08:59:32 +0000

    register Book model in Django admin interface

--- library_app/books/admin.py
+++ library_app/books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+
+# This will allow the Books model to be managed through the Django admin interface.
+admin.site.register(Book)
